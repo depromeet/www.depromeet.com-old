ad86641bf596902c0ffe6370b1f3747e9b77c9f0
권재원 <springkjw@gmail.com>
2019-02-20 12:40:13 +0900

    구글 SEO를 위해 Meta 태그 추가

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
       registry: undefined,
       awsProfile: 'depromeet',
       region: 'ap-northeast-2',
-      bucket: 'depromeet.com',
+      bucket: 'www.depromeet.com',
       createBucket: false,
       staticHosting: false,
       assetPath: 'dist',
@@ -27,7 +27,7 @@ module.exports = {
       cloudfrontId: 'E1Y2KVUUVUA1ND',
       cloudfrontMatchers: '/*',
       uploadConcurrency: 5,
-      pluginVersion: '3.0.0'
-    }
-  }
+      pluginVersion: '3.0.0',
+    },
+  },
 };
