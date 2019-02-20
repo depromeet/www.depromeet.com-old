77ec16cfc43854310e10f61e7603691da57668bc
권재원 <springkjw@gmail.com>
2019-02-20 10:52:47 +0900

    S3 배포 패키지 설치

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,26 @@ module.exports = {
       .use('vue-svg-loader')
       .loader('vue-svg-loader');
   },
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'depromeet',
+      region: 'ap-northeast-2',
+      bucket: 'depromeet.com',
+      createBucket: false,
+      staticHosting: false,
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: true,
+      pwaFiles: 'service-worker.js',
+      enableCloudfront: true,
+      cloudfrontId: 'E1Y2KVUUVUA1ND',
+      cloudfrontMatchers: '/*',
+      uploadConcurrency: 5,
+      pluginVersion: '3.0.0'
+    }
+  }
 };
